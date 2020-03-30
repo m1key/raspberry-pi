a2f06376df5b7e0a71ffa22b3f4b39d94c2a4c64
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2020-03-30 18:23:44 +0100

    Light detector.

---
+++ Nightlamp2.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+#############################################################################
+# Filename    : Nightlamp.py
+# Description : Control LED with Photoresistor
+# Author      : www.freenove.com
+# modification: 2019/12/27
+########################################################################
+import RPi.GPIO as GPIO
+import smbus
+import time
+
+address = 0x48
+bus=smbus.SMBus(1)
+cmd=0x40
+ledPin = 11 # define ledPin
+
+def analogRead(chn):
+    value = bus.read_byte_data(address,cmd+chn)
+    return value
+    
+def analogWrite(value):
+    bus.write_byte_data(address,cmd,value)  
+
+def setup():
+    global p
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ledPin,GPIO.OUT)   # set ledPin to OUTPUT mode
+    GPIO.output(ledPin,GPIO.LOW)
+    
+    p = GPIO.PWM(ledPin,1000) # set PWM Frequence to 1kHz
+    p.start(0)
+    
+def loop():
+    while True:
+        value = analogRead(0)    # read the ADC value of channel 0
+#        p.ChangeDutyCycle(value*100/255)
+        voltage = value / 255.0 * 3.3
+        if value < 50:
+            p.ChangeDutyCycle(0)
+        else:
+            p.ChangeDutyCycle(value*100/255)
+        print ('ADC Value : %d, Voltage : %.2f'%(value,voltage))
+        time.sleep(0.01)
+
+def destroy():
+    bus.close()
+    GPIO.cleanup()
+    
+if __name__ == '__main__':   # Program entrance
+    print ('Program is starting ... ')
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:  # Press ctrl-c to end the program.
+        destroy()
+        
+    
